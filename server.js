190c82f3826f69693564e287723d16d15bce8a35
Oliver Juhasz <oliver.juhasz@outlook.com>
2019-02-05 19:23:21 +0100

    add check to see if server is working

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connection: {
 app.use(bodyParser.json());
 app.use(cors())
 
-
+app.get("/", (req, res) => {res.send("it is working!")});
 
 app.post("/signin", (req, res) => {signin.handleSignin(req, res, db, bcrypt)});
 
